e2e97f34f1ad0eb579015a51e0125d2615c820cc
Eros Balta <eros_balta94@hotmail.com>
2023-10-10 02:15:59 -0300

    Add product detail to index and fix css asides

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCardContainer = document.querySelector('#shoppingCardContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,22 +11,22 @@ menuHamIcon.addEventListener('click', toggleMobileMenu );
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCardContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCardContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCardContainer.classList.contains('inactive');
    
 
     
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCardContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -43,7 +43,7 @@ function toggleCarritoAside() {
         desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCardContainer.classList.toggle('inactive');
 }
 
 /*
